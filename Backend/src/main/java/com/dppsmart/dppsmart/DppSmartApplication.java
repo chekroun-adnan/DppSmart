50acc0e0a4ebfee1bbd8b9e76a30a52d486ea5bc
Adnan Chekroun <adnanchekroun00@gmail.com>
2026-05-11 15:45:24 +0100

    Fixing Async Probleme

--- Backend/src/main/java/com/dppsmart/dppsmart/DppSmartApplication.java
+++ Backend/src/main/java/com/dppsmart/dppsmart/DppSmartApplication.java
@@ -3,15 +3,33 @@ package com.dppsmart.dppsmart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
-public class DppSmartApplication {
+@EnableAsync
+@EnableScheduling
+public class DppSmartApplication implements AsyncConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(DppSmartApplication.class, args);
     }
 
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("async-");
+        executor.initialize();
+        return executor;
+    }
 }
